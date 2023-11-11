5b293365eb73a8be2f840f2ac4769b58a80e175c
harshit <upadhyayharshit05@gmail.com>
2023-11-11 12:24:09 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ const App = () => {
   return (
    <>
    <Routes>
-    <Route path='/' element={<Home />}/>
-    <Route path='/login' element={<Login />}/>
+    <Route path='/home' element={<Home />}/>
+    <Route path='/' element={<Login />}/>
    </Routes>
    
    </>
