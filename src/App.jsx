13c75fa6583ed49f94b412b6eef6d05c80a175ed
harshit <upadhyayharshit05@gmail.com>
2023-11-11 11:48:09 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Login from './Forms/Login/Login'
+import Login from './Forms/Login/Login.jsx'
 import Home from './pages/Home'
 const App = () => {
   return (
